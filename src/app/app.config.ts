5bf4cbee343430bccd410e06f0ff047c7a0d7cef
vasicvanja <vanja.vasic@yahoo.com>
2025-11-07 13:53:25 +0100

    implement login component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideNgToast } from 'ng-angular-popup';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,56 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { NgToastService } from 'ng-angular-popup';
+import { Login } from '../../models/login';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import ValidateForm from '../../helpers/validate-form';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NgIf, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public loginForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private toastService: NgToastService,
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const loginObj: Login = {
+        Username: this.loginForm.get('username')?.value,
+        Password: this.loginForm.get('password')?.value
+      };
+
+      this.authService.login(loginObj).subscribe({
+        next: (res) => {
+          this.toastService.success(res.ErrorMessage);
+          this.loginForm.reset();
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          this.toastService.danger(err?.error?.ErrorMessage, 'Login Failed');
+        }
+      });
+    } else {
+      ValidateForm.validateAllFormFields(this.loginForm);
+      this.toastService.danger('Please fill in all required fields.', 'Form Invalid');
+    }
+  }
 }
