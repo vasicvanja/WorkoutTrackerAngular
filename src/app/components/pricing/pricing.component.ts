1d80b37f840c7772db4b99829ba54d1d1fba291d
vasicvanja <vanja.vasic@yahoo.com>
2025-11-10 17:08:10 +0100

    add pricing page

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
