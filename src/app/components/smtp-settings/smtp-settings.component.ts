8654df2ba3536e6dcad53cceb4f0c6e8925051c6
vasicvanja <vanja.vasic@yahoo.com>
2025-12-20 19:08:15 +0100

    implement smtp-settings component

--- src/app/components/smtp-settings/smtp-settings.component.ts
+++ src/app/components/smtp-settings/smtp-settings.component.ts
@@ -1,30 +1,111 @@
-import { NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import { NgToastService } from 'ng-angular-popup';
+import { SmtpSettings } from '../../models/smtp-settings';
 import { SmtpSettingsService } from '../../services/smtp-settings.service';
+import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { ResponseMessages } from '../../const/response-messages';
+import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-smtp-settings',
-  imports: [NgIf, RouterLink, ReactiveFormsModule],
+  selector: 'smtp-settings',
   templateUrl: './smtp-settings.component.html',
-  styleUrl: './smtp-settings.component.scss'
+  styleUrl: './smtp-settings.component.scss',
+  imports: [FormsModule, NgClass, NgIf]
 })
 export class SmtpSettingsComponent implements OnInit {
-  smtpSettingsForm!: FormGroup;
+
+  smtpSettings: SmtpSettings = new SmtpSettings();
+  smtpSettingsForm: FormGroup;
   enableSmtpSettings: boolean = false;
   enableAuthentication: boolean = false;
   enableSsl: boolean = false;
 
   constructor(
-    private formBuilder: FormBuilder,
+    private smtpSettingsService: SmtpSettingsService,
     private toastService: NgToastService,
-    private smtpSettingsService: SmtpSettingsService) {
-
+    private formBuilder: FormBuilder) {
+    this.smtpSettingsForm = this.formBuilder.group({
+      host: [''],
+      port: ['', Validators.pattern('^[0-9]*$')],
+      senderEmail: [''],
+      senderName: [''],
+      username: [''],
+      password: [''],
+      authentication: [false],
+      enableSsl: [false],
+      enableSmtpSettings: [false]
+    });
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.getSmtpSettings();
+  }
 
+  async getSmtpSettings() {
+    try {
+      const { Data, Succeeded, ErrorMessage } = await this.smtpSettingsService.getSmtpSettings();
+      if (Succeeded) {
+        this.smtpSettings = Data;
+        this.enableSmtpSettings = this.smtpSettings.EnableSmtpSettings;
+        this.enableAuthentication = this.smtpSettings.Authentication;
+        this.enableSsl = this.smtpSettings.EnableSsl;
+        this.smtpSettingsForm.patchValue({
+          id: this.smtpSettings.Id,
+          host: this.smtpSettings.Host,
+          port: this.smtpSettings.Port,
+          senderEmail: this.smtpSettings.SenderEmail,
+          senderName: this.smtpSettings.SenderName,
+          username: this.smtpSettings.Username,
+          password: this.smtpSettings.Password,
+          authentication: this.smtpSettings.Authentication,
+          enableSsl: this.smtpSettings.EnableSsl,
+          enableSmtpSettings: this.smtpSettings.EnableSmtpSettings
+        });
+        return Data;
+      } else {
+        this.toastService.danger(ErrorMessage);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updateSmtpSettings() {
+    if (!this.enableAuthentication) {
+      this.smtpSettings.Username = '';
+      this.smtpSettings.Password = '';
+    }
+    if (this.smtpSettingsForm.invalid) {
+      return;
+    }
+    try {
+      const { Succeeded, ErrorMessage } = await this.smtpSettingsService.updateSmtpSettings(this.smtpSettings);
+      if (Succeeded) {
+        this.toastService.success(ResponseMessages.Update_smtp_success);
+      } else {
+        this.toastService.danger(ErrorMessage);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  toggleAuthentication() {
+    const authenticationEnabled = this.smtpSettings.Authentication;
+    if (authenticationEnabled) {
+      this.enableAuthentication = true;
+    } else {
+      this.enableAuthentication = false;
+    }
+  }
+
+  toggleSmtpSettings() {
+    const smtpSettingsEnabled = this.smtpSettings.EnableSmtpSettings;
+    if (smtpSettingsEnabled) {
+      this.enableSmtpSettings = true;
+    } else {
+      this.enableSmtpSettings = false;
+    }
   }
 }
--- src/app/models/smtp-settings.ts
+++ src/app/models/smtp-settings.ts
@@ -1,5 +1,5 @@
 export class SmtpSettings {
-    public Id: number | null;
+    public Id: number;
     public Username: string;
     public Password: string;
     public SenderEmail: string;
@@ -10,16 +10,26 @@ export class SmtpSettings {
     public EnableSsl: boolean;
     public EnableSmtpSettings: boolean;
 
-    constructor(Id: number | null, Username: string, Password: string, SenderEmail: string, SenderName: string, Host: string, Port: number, Authentication: boolean, EnableSsl: boolean, EnableSmtpSettings: boolean) {
-        this.Id = Id;
-        this.Username = Username;
-        this.Password = Password;
-        this.SenderEmail = SenderEmail;
-        this.SenderName = SenderName;
-        this.Host = Host;
-        this.Port = Port;
-        this.Authentication = Authentication;
-        this.EnableSsl = EnableSsl;
-        this.EnableSmtpSettings = EnableSmtpSettings;
+    constructor(
+        Id: number = 0,
+        Username: string = "",
+        Password: string = "",
+        SenderEmail: string = "",
+        SenderName: string = "",
+        Host: string = "",
+        Port: number = 0,
+        Authentication: boolean = false,
+        EnableSsl: boolean = false,
+        EnableSmtpSettings: boolean = false) {
+            this.Id = Id;
+            this.Username = Username;
+            this.Password = Password;
+            this.SenderEmail = SenderEmail;
+            this.SenderName = SenderName;
+            this.Host = Host;
+            this.Port = Port;
+            this.Authentication = Authentication;
+            this.EnableSsl = EnableSsl;
+            this.EnableSmtpSettings = EnableSmtpSettings;
     }
 }
